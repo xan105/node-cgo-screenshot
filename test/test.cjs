68d227cd9bb6dadc7c85f4f4092cdbda249399d2
Anthony Beaumont <dev.xan105@outlook.be>
2020-12-28 00:57:19 +0700

    edge case

--- test/test.cjs
+++ test/test.cjs
@@ -2,4 +2,6 @@
 
 const screenshot = require("../lib/index.cjs");
 
+screenshot("./dump/hello World2").then(console.log).catch(console.error);
 screenshot("./dump/hello World.png").then(console.log).catch(console.error);
+screenshot("./dump/In the Name of..").then(console.log).catch(console.error);
