25684daa51b32320a14702a23f3c770b8d00a405
Anthony Beaumont <dev.xan105@outlook.be>
2025-09-25 17:55:45 +0700

    update build script

--- lib/go/build.cmd
+++ lib/go/build.cmd
@@ -2,4 +2,4 @@ cd "%~dp0src\souvenir"
 go generate
 cd "%~dp0"
 set GOPATH="%~dp0"
-go build -buildmode=c-shared -o "%~dp0\..\dist\souvenir.dll" souvenir
+go build -trimpath -buildmode=c-shared -ldflags "-w -s -buildid=" -o "%~dp0\..\dist\souvenir.dll" souvenir
